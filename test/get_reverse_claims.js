32be0e970acaa11273f195de58683b59ca943ddb
maxlath <k@maxlath.eu>
2017-03-29 10:07:45 +0200

    get_reverse_claims tests: make error messages more readible

--- test/get_reverse_claims.js
+++ test/get_reverse_claims.js
@@ -1,5 +1,4 @@
-require('should')
-
+const should = require('should')
 const getReverseClaims = require('../lib/queries/get_reverse_claims')
 
 describe('simplifyClaims', function () {
@@ -12,23 +11,23 @@ describe('simplifyClaims', function () {
     const url = getReverseClaims('P50', 'Q535')
     url.should.be.a.String()
     url.startsWith('https://query.wikidata.org').should.be.exactly(true)
-    url.match(/SELECT/).should.be.ok()
-    url.match(/WHERE/).should.be.ok()
-    url.match(/LIMIT/).should.be.ok()
-    url.match(/subject%20wdt%3AP50%20wd%3AQ535/).should.be.ok()
-    url.match(/LIMIT%201000/).should.be.ok()
+    should(url.match(/SELECT/)).be.ok()
+    should(url.match(/WHERE/)).be.ok()
+    should(url.match(/LIMIT/)).be.ok()
+    should(url.match(/subject%20wdt%3AP50%20wd%3AQ535/)).be.ok()
+    should(url.match(/LIMIT%201000/)).be.ok()
     done()
   })
 
   it('should default to limit=1000', function (done) {
     const url = getReverseClaims('P50', 'Q535')
-    url.match(/LIMIT%201000/).should.be.ok()
+    should(url.match(/LIMIT%201000/)).be.ok()
     done()
   })
 
   it('should accept an option object', function (done) {
     const url = getReverseClaims('P50', 'Q535', { limit: 500 })
-    url.match(/LIMIT%20500/).should.be.ok()
+    should(url.match(/LIMIT%20500/)).be.ok()
     done()
   })
 
@@ -36,10 +35,9 @@ describe('simplifyClaims', function () {
     const url = getReverseClaims('P2002', 'BouletCorp', { caseInsensitive: true })
     url.should.be.a.String()
     url.startsWith('https://query.wikidata.org').should.be.exactly(true)
-    url.match(/SELECT/).should.be.ok()
-    url.match(/WHERE/).should.be.ok()
-    url.match(/LIMIT/).should.be.ok()
-    url.match(/FILTER/).should.be.ok()
+    should(url.match(/SELECT/)).be.ok()
+    should(url.match(/WHERE/)).be.ok()
+    should(url.match(/LIMIT/)).be.ok()
     done()
   })
 })
