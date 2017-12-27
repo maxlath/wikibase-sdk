f69d063b45e04f7c3cb8c14f1a1f25574f048d39
maxlath <k@maxlath.eu>
2017-12-27 12:46:11 +0100

    simplify_entity: accept partail entities that is, do not throw when an entity misses one of its attributes as it might have been fetched with only some `props`

--- test/simplify_entity.js
+++ test/simplify_entity.js
@@ -1,5 +1,6 @@
 require('should')
 const Q571 = require('./data/Q571.json')
+const _ = require('lodash')
 
 const simplifyEntity = require('../lib/helpers/simplify_entity')
 
@@ -8,7 +9,8 @@ describe('simplify.entity', () => {
     simplifyEntity.should.be.a.Function()
     done()
   })
-  it('should return a simplified entity ', (done) => {
+
+  it('should return a simplified entity', (done) => {
     const simplifiedEntity = simplifyEntity(Q571)
     simplifiedEntity.labels.fr.should.equal('livre')
     simplifiedEntity.descriptions.fr.should.equal('document écrit formé de pages reliées entre elles')
@@ -19,7 +21,8 @@ describe('simplify.entity', () => {
     simplifiedEntity.sitelinks.afwiki.should.equal('Boek')
     done()
   })
-  it('should pass options down to subfunctions ', (done) => {
+
+  it('should pass options down to subfunctions', (done) => {
     const simplifiedEntity = simplifyEntity(Q571, { keepQualifiers: true, keepIds: true, addUrl: true })
     simplifiedEntity.labels.fr.should.equal('livre')
     simplifiedEntity.descriptions.fr.should.equal('document écrit formé de pages reliées entre elles')
@@ -31,6 +34,16 @@ describe('simplify.entity', () => {
     simplifiedEntity.sitelinks.afwiki.should.be.an.Object()
     simplifiedEntity.sitelinks.afwiki.title.should.equal('Boek')
     simplifiedEntity.sitelinks.afwiki.url.should.equal('https://af.wikipedia.org/wiki/Boek')
+    done()
+  })
+
+  it('should accept partial entities', (done) => {
+    const emptyEntity = simplifyEntity({})
+    Object.keys(emptyEntity).length.should.equal(3)
+    const partialEntity = simplifyEntity(_.pick(Q571, 'id', 'type', 'labels'))
+    Object.keys(partialEntity).length.should.equal(4)
+    partialEntity.labels.should.be.an.Object()
+    partialEntity.labels.fr.should.equal('livre')
     done()
   })
 })
