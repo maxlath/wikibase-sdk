7dc41bba95c05fd865fd73b47a26830e9003e561
maxlath <k@maxlath.eu>
2019-03-08 17:11:05 +0100

    add update-sitelinks-languages script and update sitelinks_languages.js
    fix #43

---
+++ scripts/sitelinks_languages/generate_sitelinks_languages.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const { uniq } = require('lodash')
+
+const monolingualProjects = [
+  'commonswiki',
+  'mediawikiwiki',
+  'metawiki',
+  'specieswiki',
+  'wikidatawiki'
+]
+
+const isntMonolingualProject = site => !monolingualProjects.includes(site)
+
+const languagesCodes = require('./sites.json')
+  .filter(isntMonolingualProject)
+  .map(site => site.split(/wik(i|t)/)[0])
+
+const stringifiedArray = JSON.stringify(uniq(languagesCodes))
+  // Prevent linting errors
+  .replace(/"/g, '\'')
+  .replace(/,/g, ', ')
+
+console.log(stringifiedArray)
