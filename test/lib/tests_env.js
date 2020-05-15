82292ce2776332d74df8a2cfe6b519807de85642
maxlath <k@maxlath.eu>
2020-05-15 13:04:58 +0200

    tests_env: fix buildUrl

--- test/lib/tests_env.js
+++ test/lib/tests_env.js
@@ -1,7 +1,8 @@
 const instance = 'https://www.wikidata.org'
+const instanceApiEndpoint = `${instance}/w/api.php`
 
 module.exports = {
   instance,
   sparqlEndpoint: 'https://query.wikidata.org/sparql',
-  buildUrl: require('../../lib/utils/build_url')(instance)
+  buildUrl: require('../../lib/utils/build_url')(instanceApiEndpoint)
 }
