7208c9812bc291c72512cdfb7501f95423a13473
maxlath <k@maxlath.eu>
2020-10-07 15:59:37 +0200

    lint: replace standard by eslint
    as standard was producing some weird errors with NodeJS 14
    
    ```
    (node:19261) Warning: Accessing non-existent property 'cat' of module exports inside circular dependency
    (Use `node --trace-warnings ...` to show where the warning was created)
    (node:19261) Warning: Accessing non-existent property 'cd' of module exports inside circular dependency
    ```

--- test/get_entity_revision.js
+++ test/get_entity_revision.js
@@ -16,7 +16,7 @@ describe('getEntityRevision', () => {
   it('should return an entity revision url [multiple args interface]', () => {
     const url = getEntityRevision('Q3548931', '3548931')
     url.should.be.a.String()
-    var [ host, query ] = url.split('?')
+    let [ host, query ] = url.split('?')
     query = qs.parse(query)
     host.should.equal('https://www.wikidata.org/w/index.php')
     query.title.should.equal('Special:EntityData/Q3548931.json')
@@ -26,7 +26,7 @@ describe('getEntityRevision', () => {
   it('should return an entity revision url [object interface]', () => {
     const url = getEntityRevision({ id: 'Q3548931', revision: '3548931' })
     url.should.be.a.String()
-    var [ host, query ] = url.split('?')
+    let [ host, query ] = url.split('?')
     query = qs.parse(query)
     host.should.equal('https://www.wikidata.org/w/index.php')
     query.title.should.equal('Special:EntityData/Q3548931.json')
