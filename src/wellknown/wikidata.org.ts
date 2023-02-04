6e58bf08b6d2bc4a0b86cdc7a79e6ef135991778
maxlath <k@maxlath.eu>
2023-02-04 22:51:32 +0100

    wikidata.org wbk: export named wdk alias

--- src/wellknown/wikidata.org.ts
+++ src/wellknown/wikidata.org.ts
@@ -1,6 +1,6 @@
 import WBK from '../wikibase-sdk.js'
 
-const wdk = WBK({
+export const wdk = WBK({
   instance: 'https://www.wikidata.org',
   sparqlEndpoint: 'https://query.wikidata.org/sparql',
 })
