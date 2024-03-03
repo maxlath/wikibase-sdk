740a737198dd5e7987f8d87ba6e6ee33d2aa6e15
maxlath <k@maxlath.eu>
2024-03-03 21:56:17 +0100

    parseSnak: replace void type with undefined
    as recommended in https://github.com/maxlath/wikibase-sdk/pull/122#discussion_r1429144499

--- src/helpers/parse_snak.ts
+++ src/helpers/parse_snak.ts
@@ -108,7 +108,7 @@ for (const [ datatype, parser ] of Object.entries(parsers)) {
   normalizedParsers[normalizeDatatype(datatype)] = parser
 }
 
-export function parseSnak (datatype: DataType | void, datavalue: SnakValue, options: SimplifySnakOptions) {
+export function parseSnak (datatype: DataType | undefined, datavalue: SnakValue, options: SimplifySnakOptions) {
   // @ts-expect-error Known case of missing datatype: form.claims, sense.claims, mediainfo.statements
   datatype = datatype || datavalue.type
 
