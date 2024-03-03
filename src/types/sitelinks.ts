ad69e3496eec1f76b5295268d0c149a9f48bf28f
maxlath <k@maxlath.eu>
2024-03-03 21:53:40 +0100

    types: add SitelinkBadges type

--- src/types/sitelinks.ts
+++ src/types/sitelinks.ts
@@ -2,10 +2,12 @@ import type { ItemId } from './entity.js'
 import type { Site } from '../helpers/wikimedia_constants.js'
 import type { Url } from '../utils/build_url.js'
 
+export type SitelinkBadges = ItemId[]
+
 export interface Sitelink {
   site: Site
   title: string
-  badges: ItemId[]
+  badges: SitelinkBadges
   url?: Url
 }
 
