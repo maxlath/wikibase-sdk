4880331dd0010858418ef6c00491a063c366f1ff
maxlath <k@maxlath.eu>
2025-09-05 16:28:01 +0200

    helpers: improve getEntityIdFromGuid types

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -73,18 +73,18 @@ export function getImageUrl (filename: string, width?: number): Url {
   return url
 }
 
-export function getEntityIdFromGuid (guid: Guid | GuidAltSyntax): EntityWithClaims['id'] {
+export function getEntityIdFromGuid <ID extends EntityWithClaims['id']> (guid: Guid<ID> | GuidAltSyntax<ID>): ID {
   const parts = guid.split(/[$-]/)
   if (parts.length === 6) {
     // Examples:
     // - q520$BCA8D9DE-B467-473B-943C-6FD0C5B3D02C
     // - P6216-a7fd6230-496e-6b47-ca4a-dcec5dbd7f95
-    return parts[0].toUpperCase() as EntityWithClaims['id']
+    return parts[0].toUpperCase() as ID
   } else if (parts.length === 7) {
     // Examples:
     // - L525-S1$66D20252-8CEC-4DB1-8B00-D713CFF42E48
     // - L525-F2-52c9b382-02f5-4413-9923-26ade74f5a0d
-    return parts.slice(0, 2).join('-').toUpperCase() as EntityWithClaims['id']
+    return parts.slice(0, 2).join('-').toUpperCase() as ID
   } else {
     throw new Error(`invalid guid: ${guid}`)
   }
