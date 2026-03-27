34d024930759207236abbfac75aeda9e5af7d1c2
ogroppo <orlando.groppo@gmail.com>
2026-03-27 22:13:33 +0000

    refactor: integrate `buildClient` into `buildSimpleClient` for improved structure and functionality

--- src/simple_client.ts
+++ src/simple_client.ts
@@ -1,8 +1,9 @@
-import { fetchJson } from './client.js'
-import { pagesTitles, type CirrusSearchPagesResponse, type RevisionsResponse, type Titles, type WbGetEntitiesResponse } from './helpers/parse_responses.js'
+import { buildClient } from './client.js'
+import { pagesTitles } from './helpers/parse_responses.js'
 import { simplifyEntities } from './helpers/simplify_entity.js'
 import { simplifySparqlResults } from './helpers/simplify_sparql_results.js'
 import type { ClientUrlBuilders } from './client.js'
+import type { RevisionsResponse, Titles, WbGetEntitiesResponse } from './helpers/parse_responses.js'
 import type { CirrusSearchPagesOptions } from './queries/cirrus_search.js'
 import type { GetEntitiesOptions } from './queries/get_entities.js'
 import type { GetEntitiesFromSitelinksOptions } from './queries/get_entities_from_sitelinks.js'
@@ -14,7 +15,7 @@ import type { SearchEntitiesOptions } from './queries/search_entities.js'
 import type { SimplifiedEntities } from './types/entity.js'
 import type { ClientOptions, SimplifyEntityOptions } from './types/options.js'
 import type { SearchResponse } from './types/search.js'
-import type { SimplifiedSparqlResults, SparqlResults } from './types/sparql.js'
+import type { SimplifiedSparqlResults } from './types/sparql.js'
 
 export interface WbkSimpleClient {
   readonly searchEntities: (options: SearchEntitiesOptions) => Promise<SearchResponse>
@@ -29,40 +30,21 @@ export interface WbkSimpleClient {
 }
 
 export function buildSimpleClient (urlBuilders: ClientUrlBuilders, clientOptions?: ClientOptions, simplifyEntityOptions?: SimplifyEntityOptions): WbkSimpleClient {
-  const {
-    searchEntities,
-    cirrusSearchPages,
-    getEntities,
-    getManyEntities,
-    getRevisions,
-    getEntityRevision,
-    getEntitiesFromSitelinks,
-    sparqlQuery,
-    getReverseClaims,
-  } = urlBuilders
-
-  const fetch = <T>(url: string) => fetchJson<T>(url, clientOptions)
+  const client = buildClient(urlBuilders, clientOptions)
   const simplify = (entities: Parameters<typeof simplifyEntities>[0]) => simplifyEntities(entities, simplifyEntityOptions)
 
   return {
-    searchEntities: options => fetch<SearchResponse>(searchEntities(options)),
-    cirrusSearchPages: async options => pagesTitles(await fetch<CirrusSearchPagesResponse>(cirrusSearchPages(options))),
-    getEntities: async options => simplify((await fetch<WbGetEntitiesResponse>(getEntities(options))).entities),
+    searchEntities: options => client.searchEntities(options),
+    cirrusSearchPages: async options => pagesTitles(await client.cirrusSearchPages(options)),
+    getEntities: async options => simplify((await client.getEntities(options)).entities),
     getManyEntities: async options => {
-      const urls = getManyEntities(options)
-      const responses = await Promise.all(urls.map(url => fetch<WbGetEntitiesResponse>(url)))
-      return responses.reduce<{ entities: SimplifiedEntities, errors: WbGetEntitiesResponse['error'][] }>(
-        (acc, { entities, error }) => ({
-          entities: { ...acc.entities, ...simplify(entities) },
-          errors: error ? [ ...acc.errors, error ] : acc.errors,
-        }),
-        { entities: {} as SimplifiedEntities, errors: [] }
-      )
+      const { entities, errors } = await client.getManyEntities(options)
+      return { entities: simplify(entities), errors }
     },
-    getRevisions: options => fetch<RevisionsResponse>(getRevisions(options)),
-    getEntityRevision: async options => simplify((await fetch<WbGetEntitiesResponse>(getEntityRevision(options))).entities),
-    getEntitiesFromSitelinks: async options => simplify((await fetch<WbGetEntitiesResponse>(getEntitiesFromSitelinks(options))).entities),
-    sparqlQuery: async sparql => simplifySparqlResults(await fetch<SparqlResults>(sparqlQuery(sparql))),
-    getReverseClaims: async options => simplifySparqlResults(await fetch<SparqlResults>(getReverseClaims(options))),
+    getRevisions: options => client.getRevisions(options),
+    getEntityRevision: async options => simplify((await client.getEntityRevision(options)).entities),
+    getEntitiesFromSitelinks: async options => simplify((await client.getEntitiesFromSitelinks(options)).entities),
+    sparqlQuery: async sparql => simplifySparqlResults(await client.sparqlQuery(sparql)),
+    getReverseClaims: async options => simplifySparqlResults(await client.getReverseClaims(options)),
   }
 }
